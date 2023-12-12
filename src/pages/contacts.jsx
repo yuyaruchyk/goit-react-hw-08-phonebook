2c4d395f612b1de9eeac5e2f91bb3a3cf18eb645
yuyaruchyk <130397548+yuyaruchyk@users.noreply.github.com>
2023-12-12 22:10:54 +0100

    add pages

---
+++ src/pages/contacts.jsx
@@ -0,0 +1,22 @@
+import { Container } from 'components/App.styled';
+import { ContactForm } from 'components/ContactForm';
+import { ContactList } from 'components/ContactList';
+import { Filter } from 'components/Filter';
+
+export function Phonebook() {
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <Container>
+        <h2>Add new contact</h2>
+        <ContactForm />
+      </Container>
+
+      <Container>
+        <h2>Contacts</h2>
+        <Filter />
+        <ContactList />
+      </Container>
+    </div>
+  );
+}
