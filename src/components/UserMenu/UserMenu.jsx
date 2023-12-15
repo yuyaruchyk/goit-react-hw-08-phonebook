5d1b3c3725730a6de1b2e7dac3727a557232a8c6
yuyaruchyk <130397548+yuyaruchyk@users.noreply.github.com>
2023-12-15 09:53:50 +0100

    fix logout

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,6 +1,7 @@
 import { useDispatch } from 'react-redux';
 import { logOut } from 'redux/auth/authOperations';
 import { useAuth } from 'hooks';
+import { useNavigate } from 'react-router-dom';
 
 import { Btn, StyledDiv, UserName } from './UserMenu.styled';
 import { StyledNavLink } from './UserMenu.styled';
@@ -8,9 +9,11 @@ import { StyledNavLink } from './UserMenu.styled';
 export const UserMenu = () => {
   const dispatch = useDispatch();
   const { user } = useAuth();
+  const navigate = useNavigate();
 
   const handleLogout = () => {
     dispatch(logOut());
+    navigate('/');
   };
 
   return (
